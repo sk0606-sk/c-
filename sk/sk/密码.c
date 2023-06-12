8d4c611a541959a36bc11e37f477f3b19c1cb83d
sk0606-sk <1791644063@qq.com>
2023-06-12 10:25:38 +0800

    6/12

---
+++ sk/sk/密码.c
@@ -0,0 +1,26 @@
+//#define _SCL_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main() {
+//	//定义一个密码的变量 不要初始值
+//	int a;
+//	char password[20] = {0};
+//	//输入密码 char %c
+//	printf("输入密码");
+//	scanf_s("%s", password);
+//	//判断 只有三次机会 字符的判断strcmp
+//	for (a = 1; a <= 3; a++) {
+//		if (strcmp(password, "123456")==0) {
+//			printf("密码正确");
+//			break;
+//		}
+//		else {
+//			printf("密码错误");
+//
+//		}
+//
+//	}
+//	if (a > 3) {
+//		printf("已经错误三次");
+//	}
+//
+//}
---
+++ sk/sk/最大公约数.c
@@ -0,0 +1,30 @@
+﻿#define _SCL_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() {
+	int a = 12, b = 18;
+	int remainder; // 余数
+	while (b != 0) {
+		//具体逻辑就是取余后小的数作为被除数 大的作为除数 所以要进行重新赋值 直到被除数为0时 除数就是最大公约数 这就是辗转相除法 
+		//12%18....12  18%12...6 被除数做除数 余数做被除数
+		remainder = a % b; // 求余数
+		a = b; // 更新a，b的值
+		b = remainder;
+		printf("A是%d%是B%d=%d\n", a, b, a % b);
+
+	}
+	/*
+a=12
+b=18
+yushu=a%b=12
+a=18
+b=yushu12
+yushu=a%b=6
+a=12
+b=yushu6
+yushu=a%b=0
+a=6
+b=0*/
+	printf("12和18的最大公约数是：%d", a);
+
+	return 0;
+}
