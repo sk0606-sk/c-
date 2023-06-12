13a9e096ef5a940bc7203cc961ac82611056e9d1
sk0606-sk <1791644063@qq.com>
2023-06-12 21:15:43 +0800

    6-12

---
+++ sk/sk/函数.c
@@ -0,0 +1,33 @@
+﻿//#define _SCL_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////int get_max(int a, int b) {
+////	return(a > b) ? (a) : (b); 
+////
+////
+////}
+////int main() {
+////	int a = 10;
+////	int b = 20;
+////	int max=get_max(a, b);
+////	printf("%d", max);
+////	return 0;
+////
+////
+////}
+//void thuan(int* a, int* b) {
+//	int tmp = 0;
+//	tmp = *a;
+//	*a = *b;
+//	*b = tmp;
+//
+//
+//}//int *a=&b 指针   *a 解地址
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	//因为 如果不把变量的地址作为参数传入的话 不能达到替换的效果 因为函数自己会自己生成自己的空间
+//	printf("a=%d,b=%d\n", a, b);
+//	thuan(&a, &b);
+//	printf("a=%d,b=%d", a, b);
+//
+//}
---
+++ sk/sk/素数.c
@@ -0,0 +1,37 @@
+﻿#define _SCL_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int is_prime(int n) {
+	int a;
+	int b = 1;
+	for (a = 2; a < n; a++) {
+		if (n %a == 0) {
+			int b = 0;
+			break;
+		}
+		if (b==1)
+		{
+			return 1;
+		}
+
+	}
+
+
+
+}
+
+
+
+int main() {
+	//打印100-200之间的素数
+	int i = 0;
+	for (i = 100; i <= 200; i++) {
+		//判断i是否为素数
+		if (is_prime(i) == 1) {
+			printf("%d\n", i);
+		}
+
+
+	}
+	return 0;
+
+}
