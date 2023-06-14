4fde1677b4a11970c73719def0b2f1b061eb1275
sk0606-sk <1791644063@qq.com>
2023-06-14 10:49:31 +0800

    6/14

---
+++ sk/sk/阶乘.cpp
@@ -0,0 +1,35 @@
+﻿#define _SCL_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	int sum = 0;
+//	int b;
+//	int n ;
+////	for (n = 1; n <= 10; n++) {
+////		int a = 1;
+////		for (b = 1; b <n; b++) {
+////			a =a*b;
+////		}
+////		sum =sum+(a*n);
+////	}
+////	
+////	printf("%d", sum);
+////}
+//int main() {
+//	int sum = 0;
+//	int b;
+//	int n=3;
+//	int a = 1;
+//		for (b = 1; b <=n; b++) {
+//			//因为在循环中 a的值不会重新被赋值 所以当b等于1的时候a的值为1！b为2的时候a的值为2！所以阶乘可以表示为a*b
+//			a = a*b;
+//			sum = sum + a;
+//		}
+//	
+//
+//		printf("%d", sum);
+//	}
+//
+//
+//	
+//		
+
